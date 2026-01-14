87acd2185a01b98ac68b8ce71773f503954c7da4
ArnaudPantaleon <128701834+ArnaudPantaleon@users.noreply.github.com>
2026-01-14 22:58:35 +0100

    Change fetchSuggestions to async function

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ window.selectSuggestion = (city, lat, lon, idx) => {
 };
 
 // Récupérer les suggestions avec débounce
-function fetchSuggestions(query) {
+async function fetchSuggestions(query) {
     const response = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${query}&limit=15&type=municipality`);
     const data = await response.json();
     displaySuggestions(data.features); // Attention : on passe data.features

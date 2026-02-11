e285dff23a61f233815ffc5c8b11f88f472bbeda
ArnaudPantaleon <128701834+ArnaudPantaleon@users.noreply.github.com>
2026-02-11 09:18:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const CONFIG = {
     // === FILTRES PAR STATUT (Couleurs) ===
     status: {
         "Ouvert": { color: "#009597", label: "Cabinets Omedys", description: "Ouverts", checked: true },
-        "Ouvertes": { color: "#2ecc71", label: "Salles Ouvertes", description: "485 sites", checked: true },
+        "Ouvertes": { color: "#3498db", label: "Salles Ouvertes", description: "485 sites", checked: true },
         "Telesecretariat OMEDYS": { color: "#8956FB", label: "Téléscrétariat", description: "89 sites", checked: true },
-        "Ouverture en cours": { color: "#3498db", label: "En cours d'ouverture", description: "142 sites", checked: false },
+        "Ouverture en cours": { color: "#2ecc71", label: "En cours d'ouverture", description: "142 sites", checked: false },
         "Fermees ou refus OTT": { color: "#f17676", label: "Fermées", description: "89 sites", checked: false },
         "Inactives": { color: "#cbd5e1", label: "Fermées", description: "89 sites", checked: false },
         "En sourcing": { color: "#fdaf00", label: "En sourcing", description: "89 sites", checked: false }

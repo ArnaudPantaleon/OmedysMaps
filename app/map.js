a3e4fd9644dff45302bd5b99873a76b9bc6619d1
ArnaudPantaleon <128701834+ArnaudPantaleon@users.noreply.github.com>
2026-02-23 12:10:41 +0100

    Update map.js

--- app/map.js
+++ app/map.js
@@ -2,49 +2,55 @@ import {CONFIG} from "../core/config.js"
 import {store} from "../core/store.js"
 import {BP3Popup} from "../ui/popup.bp3.js"
 import {updateStats} from "../ui/ui.stats.js"
-export class MapEngine{
+
+export class MapEngine {
 
 constructor(){
-  this.map=L.map("map",{zoomControl:false})
+
+  this.map = L.map("map",{zoomControl:false})
   .setView(CONFIG.map.center,CONFIG.map.zoom)
-  
+
   L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   ).addTo(this.map)
-  
+
   this.cluster = L.markerClusterGroup()
   this.map.addLayer(this.cluster)
 
-
-  renderSites(){
-  
-  store.sites.forEach(site=>{
-  
-  const color = CONFIG.status[site.statut]?.color || "#94a3b8"
-  
-  const marker=L.circleMarker(
-  [site.lat,site.lng],
-  {
-  radius:7,
-  fillColor:color,
-  color:"#fff",
-  weight:2,
-  fillOpacity:.9
-  })
-  
-  marker.site = site
-  
-  marker.bindPopup(
-  BP3Popup(site,color),
-  {maxWidth:340}
-  )
-  
-  this.cluster.addLayer(marker)
-  
-  store.markers.push(marker)
-  
-  })
-  updateStats()
-  }
 }
 
+renderSites(){
+
+  store.sites.forEach(site=>{
+
+    const color = CONFIG.status[site.statut]?.color || "#94a3b8"
+
+    const marker = L.circleMarker(
+      [site.lat,site.lng],
+      {
+        radius:7,
+        fillColor:color,
+        color:"#fff",
+        weight:2,
+        fillOpacity:.9
+      }
+    )
+
+    marker.site = site
+
+    marker.bindPopup(
+      BP3Popup(site,color),
+      {maxWidth:340}
+    )
+
+    this.cluster.addLayer(marker)
+
+    store.markers.push(marker)
+
+  })
+
+  updateStats()
+
+}
+
+}

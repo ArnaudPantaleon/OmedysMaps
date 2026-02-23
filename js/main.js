e9db8265269c87d089b9082b0d692292ff99bebf
ArnaudPantaleon <128701834+ArnaudPantaleon@users.noreply.github.com>
2026-02-23 18:40:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 import { loadData }   from "./data/data.loader.js"
 import { MapEngine }  from "./map/map.engine.js"
 import { initFilters } from "./ui/ui.filters.js"
+import { initSearch }  from "./ui/ui.search.js"
 import { initStats }   from "./ui/ui.stats.js"
 
 async function start() {
 
+  // Charger les données sites
   await loadData()
 
+  // Charger zones (régions + départements) pour les filtres géo
+  const zones = await fetch("/data/zones.json").then(r => r.json()).catch(() => null)
+  window._zonesCache = zones  // cache pour le reset des filtres
+
+  // Carte
   const map = new MapEngine()
   map.renderSites()
 
-  initFilters(map)
+  // UI
+  initFilters(map, zones)
+  initSearch(map)
   initStats()
 
 }

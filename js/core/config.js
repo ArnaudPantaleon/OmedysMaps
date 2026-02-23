035ab7f0f098099a5019c3c48c0960751bc2866c
ArnaudPantaleon <128701834+ArnaudPantaleon@users.noreply.github.com>
2026-02-23 13:02:11 +0100

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -9,6 +9,11 @@ status:{
 Ouvert:{color:"#16a34a",checked:true},
 "Ouverture en cours":{color:"#f59e0b",checked:true},
 Virtuel:{color:"#64748b",checked:false}
+},
+
+datasets:{
+cabinet:{color:"#0ea5e9"},
+salle:{color:"#6366f1"}
 }
 
 }
